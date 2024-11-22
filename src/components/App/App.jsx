1cecc989d15f9559d5e082aa8890b361d32163f5
daniel sida <banuelossidadaniel@gmail.com>
2024-11-22 01:01:14 -0800

    add 404 file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
   useNavigate,
@@ -158,7 +158,7 @@ function App() {
 }
 function AppWrapper() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <App />
     </Router>
   );
